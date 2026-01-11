4ebe2c8b7a070bdce149d47cfad393bed02d48fc
al7rbi1293-afk <al7rbi1293@gmail.com>
2026-01-11 11:41:25 +0300

    Update warehouse.py

--- modules/views/warehouse.py
+++ modules/views/warehouse.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import pandas as pd
 import time
 from modules.database import run_query, run_action, run_batch_action
 from modules.config import TEXT as txt, CATS_EN, LOCATIONS, EXTERNAL_PROJECTS, AREAS
